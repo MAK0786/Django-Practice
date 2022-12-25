4d6312c00cb91d7f743a6d3982ad0f3c449c941e
Arsalan <sonukhanroxxx@gmail.com>
2022-12-25 17:53:32 +0530

    'update'

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path 
+from decouple import config
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +27,7 @@ STATIC_DIR = BASE_DIR / 'static'
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gz6=fafhl7+ii7x(jy-y8tm!=z4ggtu70sngf1v+b3$jo=!b@)'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
